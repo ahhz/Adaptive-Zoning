22fffd1932c2bdeb2d45b13971795e180af4e2ef
Alex Hagen-Zanker <a.hagen-zanker@surrey.ac.uk>
2025-04-02 16:47:29 +0100

    Reducing line-length to79 characters

--- adaptive_zoning.py
+++ adaptive_zoning.py
@@ -33,26 +33,37 @@ from neighbourhood_maker import NeighbourhoodMaker
 
 class AdaptiveZoneSystem:
     """
-    Implements the adaptive zoning method as published by Hagen-Zanker and Jin (2012).
+    Implements the adaptive zoning method as published by Hagen-Zanker and 
+    Jin (2012).
 
-    This class performs hierarchical clustering of zones and then determines a
-    neighborhood of interacting zones for each initial (leaf) zone.
+    This class performs hierarchical clustering of zones and then 
+    determines a neighborhood of interacting zones for each initial (leaf) 
+    zone.
  
     """
-    def __init__(self, origins: List[float], destinations: List[float], weights: List[float],
-                 centroids: List[Tuple[float, float]], beta: float, nbh_size: int):
+    def __init__(self, 
+                 origins: List[float], 
+                 destinations: List[float], 
+                 weights: List[float],
+                 centroids: List[Tuple[float, float]], 
+                 beta: float, nbh_size: int):
         """
         Initializes the adative_zone_system.
 
         Args:
-            origins (List[float]): A list of origin values for each initial zone.
-            destinations (List[float]): A list of destination values for each initial zone.
-            weights (List[float]): A list of weight values for each initial zone.
-            points (List[Tuple[float, float]]): A list of coordinate tuples (x, y) for each initial zone,
-                                                representing their locations.
-            beta (float): A parameter influencing the clustering and neighborhood formation.
-            nbh_size (int): The desired number of (leaf and aggregated) zones that each leaf zone's
-                              neighborhood will aim to include.
+            origins (List[float]): A list of origin values for each initial 
+                zone.
+            destinations (List[float]): A list of destination values for 
+                each initial zone.
+            weights (List[float]): A list of weight values for each initial 
+                zone.
+            centroids (List[Tuple[float, float]]): A list of coordinate 
+                tuples (x, y) for each initial zone, representing their 
+                locations.
+            beta (float): A parameter influencing the clustering and 
+                neighborhood formation.
+            nbh_size (int): The desired number of (leaf and aggregated) 
+                zones that each leaf zone's neighborhood will include.
         """
         self._beta = beta
         self._nbh_size = nbh_size
@@ -60,7 +71,9 @@ class AdaptiveZoneSystem:
         clusterer = ClusterMaker(self._data, beta)
         self._zone_tree = clusterer.create()
         self._distance_matrix = clusterer.get_distance_matrix()
-        neighbourhooder = NeighbourhoodMaker(self._data, beta, nbh_size, self._zone_tree, self._distance_matrix)
+        neighbourhooder = NeighbourhoodMaker(self._data, beta, nbh_size, 
+                                             self._zone_tree, 
+                                             self._distance_matrix)
         self._neighbourhoods = neighbourhooder.create()
         self._transposed_neighbourhoods = None
 
@@ -78,15 +91,16 @@ class AdaptiveZoneSystem:
         Returns the list of neighborhoods for each initial leaf zone.
 
         Returns:
-            A list of neighborhoods, where each neighborhood is a set of integer indices
-            representing the zones (both leaf and aggregated) that the corresponding
-            initial leaf zone will interact with.
+            A list of neighborhoods, where each neighborhood is a set of 
+            integer indices representing the zones (both leaf and aggregated) 
+            that the corresponding initial leaf zone will interact with.
         """
         return self._neighbourhoods
 
     def get_beta(self) -> float:
         """
-        Returns the beta parameter used for clustering and neighborhood formation.
+        Returns the beta parameter used for clustering and neighborhood 
+        formation.
 
         Returns:
             The beta parameter (float).
@@ -98,7 +112,8 @@ class AdaptiveZoneSystem:
         Returns the neighborhood size.
 
         Returns:
-            The desired number of zones in each leaf zone's neighborhood (int).
+            The desired number of zones in each leaf zone's neighborhood 
+            (int).
         """
         return self._nbh_size
 
@@ -113,7 +128,8 @@ class AdaptiveZoneSystem:
 
     def num_zones(self) -> int:
         """
-        Returns the total number of zones in the hierarchical tree (including aggregated zones).
+        Returns the total number of zones in the hierarchical tree 
+        (including aggregated zones).
 
         Returns:
             The total number of zones (int).
@@ -124,24 +140,27 @@ class AdaptiveZoneSystem:
         """
         Returns the transposed neighborhood list.
 
-        This list indicates for each zone (by its index in the hierarchical tree),
-        which initial leaf zones consider it part of their neighborhood.
+        This list indicates for each zone (by its index in the hierarchical
+        tree), which initial leaf zones have it in their neighborhood.
 
         Returns:
-            A list where each inner list contains the indices of the initial leaf zones
-            that include the current zone in their neighborhood.
+            A list where each inner list contains the indices of the 
+            initial leaf zones that include the current zone in their 
+            neighborhood.
         """
         if self._transposed_neighbourhoods is not None:
             return self._transposed_neighbourhoods
 
         num_total_zones = self.num_zones()
-        self._transposed_neighbourhoods = [[] for _ in range(num_total_zones)]
+        self._transposed_neighbourhoods = [
+            [] for _ in range(num_total_zones)]
         for i, row in enumerate(self._neighbourhoods):
             for j in row:
                 self._transposed_neighbourhoods[j].append(i)
         return self._transposed_neighbourhoods
     
-    def map_leaf_zones_to_neighbourhood(self, center:int, renumber : bool = False)->List[int]:
+    def map_leaf_zones_to_neighbourhood(self, center: int, 
+                                        renumber: bool = False)->List[int]:
         out = [0 for _ in range(self._zone_tree.get_num_leafs())]
         for index, neighbour in enumerate(self._neighbourhoods[center]):
             leafs = self._zone_tree.get_leafs(neighbour)
@@ -149,20 +168,29 @@ class AdaptiveZoneSystem:
                 out[leaf] = index if renumber else neighbour
         return out
     
-    def map_leaf_zones_to_n_clusters(self, n : int, renumber : bool = False)->List[int]:
+    def map_leaf_zones_to_n_clusters(self, n: int, 
+                                     renumber: bool = False)->List[int]:
         return self._zone_tree.map_leafs_to_n_groups(n, renumber)
    
-    def plot_n_clusters_voronoi(self, n : int, ax: Optional[matplotlib.axes.Axes] = None) -> matplotlib.axes.Axes:
+    def plot_n_clusters_voronoi(self, n : int, 
+                                ax: Optional[matplotlib.axes.Axes] = None
+                                ) -> matplotlib.axes.Axes:
         if ax is None:
             fig, ax = plt.subplots()
         agg = self.map_leaf_zones_to_n_clusters(n, True)
-        plot_agg_voronoi(self._data.centroids[:self._zone_tree.get_num_leafs()],agg, ax)
+        num_leafs = self._zone_tree.get_num_leafs()
+        leaf_centroids = self._data.centroids[:num_leafs]
+        plot_agg_voronoi(leaf_centroids, agg, ax)
         return ax
 
-    def plot_neighbourhood_voronoi(self, center : int, ax: Optional[matplotlib.axes.Axes] = None) -> matplotlib.axes.Axes:
+    def plot_neighbourhood_voronoi(self, center : int, 
+                                   ax: Optional[matplotlib.axes.Axes] = 
+                                   None) -> matplotlib.axes.Axes:
         if ax is None:
             fig, ax = plt.subplots()
         agg = self.map_leaf_zones_to_neighbourhood(center, True)
-        plot_agg_voronoi(self._data.centroids[:self._zone_tree.get_num_leafs()], agg, ax)
+        num_leafs = self._zone_tree.get_num_leafs()
+        leaf_centroids = self._data.centroids[:num_leafs]
+        plot_agg_voronoi(leaf_centroids, agg, ax)
         return ax 
 
